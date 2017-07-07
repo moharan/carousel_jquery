5c516a39c8e5b40c76eddbed11321de8b5b2f5dd
RangiBecerra <rangibecerra@gmail.com>
2017-07-07 01:03:37 -0400

    funcionando 1/2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	alert("existe");
+	//alert("existe");
+	$("#botonera li").click(function(){
+    	var carousel = $(this).attr("carousel");
+    	console.log("carousel", carousel);
+    	$("#slider ul li").css({"display":"none"});
+    	$("#slider ul li:nth-child(" + carousel + ")").fadeIn();
+    });
 });
