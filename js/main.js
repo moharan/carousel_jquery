2ec0dbce20f967d9080889521331f8757c6c6f5b
RangiBecerra <rangibecerra@gmail.com>
2017-07-07 21:00:10 -0400

    siguiente - atras funcionando

--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
 $(document).ready(function() {
 	//alert("existe");
+    function avanzar(){
+        if(slider >=5){
+            slider = 1;
+        } else {
+            slider++;
+        }
+        console.log("slider", slider);
+        $("#slider ul li").css({"display":"none"});
+        $("#slider ul li:nth-child(" + slider + ")").fadeIn();
+        $("#botonera li").css({"color":"white"});
+        $("#botonera li:nth-child(" + slider + ")").css({"color":"purple"});
+    }
+	var slider = 1;
+	// funcionamiento al click
 	$("#botonera li").click(function(){
     	var carousel = $(this).attr("carousel");
     	console.log("carousel", carousel);
     	$("#slider ul li").css({"display":"none"});
+    	// solo se visualizara el slider con la misma posición
     	$("#slider ul li:nth-child(" + carousel + ")").fadeIn();
+    	// estado de botonera cuando este seleccionada la misma imagen
+    	$("#botonera li").css({"color":"white"});
+    	// activa botonera
+    	$(this).css({"color":"purple"});
     });
+    $("#next01").click(function(){
+    	if(slider <=1){
+    		slider = 5;
+    	} else {
+    		slider--;
+    	}
+    	
+    	console.log("slider", slider);
+        $("#slider ul li").css({"display":"none"});
+        $("#slider ul li:nth-child(" + slider + ")").fadeIn();
+        $("#botonera li").css({"color":"white"});
+        $("#botonera li:nth-child(" + slider + ")").css({"color":"purple"});
+    });
+    $("#next02").click(function(){
+        //llamando a funcion
+    	avanzar();
+    });
+    /*loop*/
+    setInterval(function(){
+        //llamando a funcion
+        avanzar();
+    },5000);
 });
